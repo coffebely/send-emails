cef881cc77c62a7afd16f161a64f74ab10cba3b4
Coffe.bely <coffe.bely@gmail.com>
2021-10-16 20:56:12 +0700

    Исправил пути к сертификатам

--- send_email/send_email.py
+++ send_email/send_email.py
@@ -6,7 +6,7 @@ from .text import text, title
 
 def send_simple_message(email, name):
     MEDIA_DIR = str(os.path.join(Path(__file__).resolve().parent.parent, 'static'))
-    attachment = os.path.join(MEDIA_DIR, f'image\certificate-{name}.jpg')
+    attachment = os.path.join(MEDIA_DIR, rf'image\certificate-{name}.jpg')
     return requests.post(
         "https://api.mailgun.net/v3/YOUR_DOMAIN/messages",
         auth=("api", "YOUR_API_KEY"),
