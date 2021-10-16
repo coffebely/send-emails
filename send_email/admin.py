614a2ff17b5cf78de2a89958d59a7e2b507c26b7
Coffe.bely <coffe.bely@gmail.com>
2021-10-16 20:47:24 +0700

    Исправлены пути к сертификатам

--- send_email/admin.py
+++ send_email/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import File, Person
 
+admin.site.register(File)
+admin.site.register(Person)
 # Register your models here.
--- send_email/send_email.py
+++ send_email/send_email.py
@@ -8,9 +8,9 @@ def send_simple_message(email,  name):
     MEDIA_DIR = str(os.path.join(Path(__file__).resolve().parent.parent, 'media'))
     attachment = os.path.join(MEDIA_DIR, f'certificate-{name}.jpg')
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox9533174d59624751a515752235e0f2c3.mailgun.org/messages",
-        auth=("api", "b4067df9b14b6b473a0865341d43fc82-443ec20e-0a52789b"),
-        data={"from": "mailgun@sandbox9533174d59624751a515752235e0f2c3.mailgun.org",
+        "https://api.mailgun.net/v3/YOUR_DOMAIN/messages",
+        auth=("api", "YOUR_API_KEY"),
+        data={"from": "YOUR_DOMAIN",
               "to": [f"{email}"],
               "subject": title,
               "text": text},
